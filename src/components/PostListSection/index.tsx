f6918d35cbd5d2166930aa266e8eb4ca4b843791
iyfhan <iyfhan@gmail.com>
2025-06-12 12:29:58 +0900

    Update index.tsx

--- src/components/PostListSection/index.tsx
+++ src/components/PostListSection/index.tsx
@@ -1,6 +1,5 @@
 import Card from "../Card";
 import Sidebar from "../Sidebar";
-
 import sortDatesDescending from "@/utils/sortDatesDescending";
 import { allPosts } from "contentlayer/generated";
 import Link from "next/link";
@@ -9,12 +8,10 @@ import Link from "next/link";
 const PostListSection = ({ category }: { category?: string }) => {
   return (
     <section className="flex w-full max-w-[1024px] relative justify-center mx-auto gap-4 p-4 py-16">
-      <div className="w-64 h-fit sticky top-16 hidden desktop:flex shrink-0">
-        <Sidebar />
-      </div>
+
       <div className="w-full flex flex-col gap-8">
         <h2 className=" font-black text-textColor text-6xl">
-          {category ?? "All"}
+          {category ?? "GOODMIND"}
           <span className="text-primary text-center items-center justify-center text-[5rem] leading-[0]">
             .
           </span>
@@ -35,6 +32,11 @@ const PostListSection = ({ category }: { category?: string }) => {
             ))}
         </div>
       </div>
+
+      <div className="w-64 h-fit sticky top-16 hidden desktop:flex shrink-0">
+        <Sidebar />
+      </div>
+
     </section>
   );
 };
