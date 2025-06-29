28c166ada1b6f35bbb728448db17671309ee1d72
iyfhan <iyfhan@gmail.com>
2025-06-29 09:56:24 +0900

    Update index.tsx

--- src/components/Adsense/index.tsx
+++ src/components/Adsense/index.tsx
@@ -1,18 +1,18 @@
-'use client';
+// 'use client';
 
-      export default function Adsense() {
-        return (
-          <div>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1638869731496989"
-     crossorigin="anonymous"></script>
-{/* <!-- ads6 --> */}
-<ins class="adsbygoogle"
-     style="display:inline-block;width:200px;height:500px"
-     data-ad-client="ca-pub-1638869731496989"
-     data-ad-slot="3000795090"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-          </div>
-        );
-      }
+//       export default function Adsense() {
+//         return (
+//           <div>
+//             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1638869731496989"
+//      crossorigin="anonymous"></script>
+// {/* <!-- ads6 --> */}
+// <ins class="adsbygoogle"
+//      style="display:inline-block;width:200px;height:500px"
+//      data-ad-client="ca-pub-1638869731496989"
+//      data-ad-slot="3000795090"></ins>
+// <script>
+//      (adsbygoogle = window.adsbygoogle || []).push({});
+// </script>
+//           </div>
+//         );
+//       }
